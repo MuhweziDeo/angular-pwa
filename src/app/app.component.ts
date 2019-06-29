7f29a7f6457b3f260bd88c93d9710b9db9a2906c
muhwezi deo <aggrey256@gmail.com>
2019-06-29 23:13:38 +0300

    setup basic pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-pwa-app';
+  joke: any;
+
+  constructor(updates: SwUpdate, private dataService: DataService) {
+       updates.available.subscribe(() => {
+         updates.activateUpdate().then(() => document.location.reload());
+       });
+  }
+
+  ngOnInit() {
+    this.dataService.gimmeJokes().subscribe(res => {
+        this.joke = res;
+    });
+  }
+
 }
